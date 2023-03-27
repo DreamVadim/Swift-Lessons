b7d0954302d4486cc513f732f30dcfd7a440ef6d
Vadim Mihailov <picturax@icloud.com>
2023-03-27 22:36:34 +0300

    Started do lesson 3

--- Swift - Alex Skutaenko/SwiftLesson03.playground/Contents.swift
+++ Swift - Alex Skutaenko/SwiftLesson03.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+// Моя физическая подготовка
+let myPhysicalFitness = (pushupsMax: 50, pullupsMax: 10, squatsMax: 60)
+print(myPhysicalFitness, "\n")
 
-var greeting = "Hello, playground"
+print("My physical fitness")
+print("1. Maximum push-ups: \(myPhysicalFitness.pushupsMax)")   //Value access via parameter
+print("2. Maximum pull-ups: \(myPhysicalFitness.1)")            //Value access via index
+print("3. Maximum squats: \(myPhysicalFitness.squatsMax)")      //Value access via parameter
+print("\n\n")
+
+// Физическая подготовка моей девушки
+var gfPhysicalFitness = myPhysicalFitness
+gfPhysicalFitness.pushupsMax = 20
+gfPhysicalFitness.pullupsMax = 5
+gfPhysicalFitness.squatsMax = 40
+print(gfPhysicalFitness, "\n")
+
+print("My girlfriend physical fitness")
+print("1. Maximum push-ups: \(gfPhysicalFitness.pushupsMax)")
+print("2. Maximum pull-ups: \(gfPhysicalFitness.1)")
+print("3. Maximum squats: \(gfPhysicalFitness.squatsMax)")
+print("\n\n")
